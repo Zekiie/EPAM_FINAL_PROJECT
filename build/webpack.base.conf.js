63c3167b739e34589b4eb0871b4b63e5be60b010
Zekiie Irikh <zekie.irikh@gmail.com>
2019-03-29 06:34:50 +0200

    first commit

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.resolve(__dirname, '../src'),
+  dist: path.resolve(__dirname, '../dist'),
+  app: 'app/'
+}
+
+module.exports = {
+  // BASE config
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `${PATHS.app}js/[name].js`,
+    path: PATHS.dist,
+    publicPath: ''
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    },{
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+        }
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.app}css/[name].css`,
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html'
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/img`, to: `${PATHS.app}img` },
+      { from: `${PATHS.src}/img/goods`, to: `${PATHS.app}img/goods` },
+
+    ])
+  ],
+}
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Main js file
+
+// another js file (example)
+import './common.js'
+import '../css/main.css'
+import '../scss/main.scss'
